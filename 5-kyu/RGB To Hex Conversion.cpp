e2ce439a44660f1bf9e818e1e4e63fe6a2c3a9fb
Đỗ Thành Đạt <71697123+Ducanger@users.noreply.github.com>
2020-09-22 22:54:40 +0700

    Update RGB To Hex Conversion.cpp

--- 5-kyu/RGB To Hex Conversion.cpp
+++ 5-kyu/RGB To Hex Conversion.cpp
@@ -9,12 +9,11 @@ class RGBToHex
 string toHexadecimal(int n)
 {
     char d[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    if ( n < 0 ) return "00";
+    if ( n > 255 ) return "FF";
     string s = "";
-    if ( n < 0 ) s += "00";
-    else if ( n < 16 ) s += '0';
-    else if ( n < 256 ) s += d[n/16];
-    else s += "FF";
-    if ( n >= 0 && n < 256 ) s += d[n%16];
+    if ( n < 16 ) { s += '0'; } else s += d[n/16];
+    s += d[n%16];
     return s;
 }
 
